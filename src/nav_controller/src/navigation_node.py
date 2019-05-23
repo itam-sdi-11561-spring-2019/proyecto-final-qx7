d10ea4ed9f21e2f08b447f07ae09a07fb152d4b6
lgarciaos <lgarciaos@protonmail.com>
2019-05-22 23:01:22 -0500

    update

--- src/nav_controller/src/navigation_node.py
+++ src/nav_controller/src/navigation_node.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import rospy
+import time
+import math
 from geometry_msgs.msg import Pose2D
+from graphical_client.msg import Pose2D_Array
 import sympy
 import sympy as sym
 from sympy.abc import s
@@ -8,15 +11,17 @@ from sympy.geometry import Point, Circle, Segment, intersection, Ray
 
 poseSelf= Pose2D()
 poseNav= Pose2D()
-traj= Pose2D_Array()
+velVector= Pose2D()
 
 def callbackTrajectory(data):
-    traj= data;
-    rospy.loginfo(data)
+    n=len(data.poses)
+    poseNav.x= int(data.poses[1].x)
+    poseNav.y= int(data.poses[1].y)
+    poseNav.theta= data.poses[1].theta
 
 def callbackSelf(dataSelf):
-    poseSelf.x = dataSelf.x
-    poseSelf.y = dataSelf.y
+    poseSelf.x = int(dataSelf.x)
+    poseSelf.y = int(dataSelf.y)
     poseSelf.theta = dataSelf.theta
 
 def navigator():
@@ -24,12 +29,22 @@ def navigator():
     rospy.Subscriber('/trajectory', Pose2D_Array, callbackTrajectory)
     rospy.Subscriber('/y_r0', Pose2D, callbackSelf)
     rospy.init_node('navigation')
-    rate= rospy.Rate(2) # Hz
-
+    rate= rospy.Rate(1) # Hz
     while not rospy.is_shutdown():
+        pSelf=Point(poseSelf.x,poseSelf.y)
+        pNav=Point(poseNav.x,poseNav.y)
+        dist= pSelf.distance(pNav)
+        s= math.atan2(poseNav.y-poseSelf.y,poseNav.x-poseSelf.x)
+        velVector.theta= s-poseSelf.theta
+        if(dist>500):
+            v= 350
+        elif(dist>100):
+            v=0.3*dist +230
+        else:
+            v=200
+        velVector.x= v
 
-        print "fsdj", traj
-        pub.publish(poseNav)
+        pub.publish(velVector)
         rate.sleep()
 
 if __name__ == '__main__':
--- src/trajectory_planner/src/planner_node.py
+++ src/trajectory_planner/src/planner_node.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+import time
 from geometry_msgs.msg import Pose2D
 from graphical_client.msg import Pose2D_Array
 import sympy
@@ -7,6 +8,7 @@ import sympy as sym
 from sympy.abc import s
 from sympy.geometry import Point, Circle,Segment, intersection, Ray
 
+radioObstaculo=180
 poseSelf= Pose2D()
 poseTarget= Pose2D()
 poseObs0= Pose2D()
@@ -17,8 +19,8 @@ poseObs4= Pose2D()
 poseObs5= Pose2D()
 poseObs6= Pose2D()
 poseObs7= Pose2D()
-newP=[]
-index=[]
+
+
 def init_pose():
     pose = Pose2D()
     pose.x = 0
@@ -77,14 +79,14 @@ def callbackObs7(dataObs):
 
 def calcCircs():
     obsCirculo=[]
-    obsCirculo.append(Circle(Point(int(poseObs0.x),int(poseObs0.y)), 180))
-    obsCirculo.append(Circle(Point(int(poseObs1.x),int(poseObs1.y)), 180))
-    obsCirculo.append(Circle(Point(int(poseObs2.x),int(poseObs2.y)), 180))
-    obsCirculo.append(Circle(Point(int(poseObs3.x),int(poseObs3.y)), 180))
-    obsCirculo.append(Circle(Point(int(poseObs4.x),int(poseObs4.y)), 180))
-    obsCirculo.append(Circle(Point(int(poseObs5.x),int(poseObs5.y)), 180))
-    obsCirculo.append(Circle(Point(int(poseObs6.x),int(poseObs6.y)), 180))
-    obsCirculo.append(Circle(Point(int(poseObs7.x),int(poseObs7.y)), 180))
+    obsCirculo.append(Circle(Point(int(poseObs0.x),int(poseObs0.y)), radioObstaculo))
+    obsCirculo.append(Circle(Point(int(poseObs1.x),int(poseObs1.y)), radioObstaculo))
+    obsCirculo.append(Circle(Point(int(poseObs2.x),int(poseObs2.y)), radioObstaculo))
+    obsCirculo.append(Circle(Point(int(poseObs3.x),int(poseObs3.y)), radioObstaculo))
+    obsCirculo.append(Circle(Point(int(poseObs4.x),int(poseObs4.y)), radioObstaculo))
+    obsCirculo.append(Circle(Point(int(poseObs5.x),int(poseObs5.y)), radioObstaculo))
+    obsCirculo.append(Circle(Point(int(poseObs6.x),int(poseObs6.y)), radioObstaculo))
+    obsCirculo.append(Circle(Point(int(poseObs7.x),int(poseObs7.y)), radioObstaculo))
     return obsCirculo;
 
 def calcIntersect(circles,segment):
@@ -99,17 +101,17 @@ def calcIntersect(circles,segment):
     intersect.append(intersection(segment, circles[7]))
     return intersect;
 
-def distanceVector(point):
+def distanceVector(point, vec):
     distances=[]
-    n=len(newP)
+    n=len(vec)
     for x in xrange(n):
-        laux=newP[x]
+        laux=vec[x]
         distances.append(point.distance(laux[0]))
 
     return distances;
-def arrange(point):
+def arrange(point, vec):
     aux=[]
-    distances= distanceVector(point)
+    distances= distanceVector(point, vec)
     m=len(distances)
     while m != 0 :
         ind_min= distances.index(min(distances))
@@ -129,11 +131,12 @@ def recalc(inter, centro,circ):
     return intersection(line,circ);
 
 def intDiscriminator(list):
-    if(len(list)==2):
-        out=2
-    else :
-        out=0
-
+    n=len(list)
+    out=0
+    for x in xrange(n):
+        aux= len(list[x])
+        if(aux==2):
+            out= out+aux
     return out
 
 def planner():
@@ -149,75 +152,75 @@ def planner():
     rospy.Subscriber('/b_r6', Pose2D, callbackObs6)
     rospy.Subscriber('/b_r7', Pose2D, callbackObs7)
     rospy.init_node('planner')
-    #rate = rospy.Rate(1) # 10hz!!!! CAMBIARLO
+    rate = rospy.Rate(1)
+    # time.sleep(1)
 
-    # while not rospy.is_shutdown():
-    pointSelf= Point(int(poseSelf.x),int(poseSelf.y))
-    pointTarget= Point(int(poseTarget.x),int(poseTarget.y))
-    arr = Pose2D_Array()  #contiene la salida del nodo
-    arr.poses.append(poseSelf) #el primer punto siempre es donde se encuentra el robot actualmente
-    obstacles= calcCircs()
-    trajectory = Segment(pointSelf,pointTarget)
-    intersections= calcIntersect(obstacles, trajectory)
+    while not rospy.is_shutdown():
+        pointSelf= Point(int(poseSelf.x),int(poseSelf.y))
+        pointTarget= Point(int(poseTarget.x),int(poseTarget.y))
+        arr = Pose2D_Array()  #contiene la salida del nodo
+        arr.poses.append(poseSelf) #el primer punto siempre es donde se encuentra el robot actualmente
+        obstacles= calcCircs()
+        trajectory = Segment(pointSelf,pointTarget)
+        intersections= calcIntersect(obstacles, trajectory)
+        numIntersect= intDiscriminator(intersections)
 
-    numIntersect= len(intersections[0])+len(intersections[1])+len(intersections[2])+len(intersections[3])+len(intersections[4])+len(intersections[5])+len(intersections[6])+len(intersections[7])
-
-    if numIntersect==0:
-        arr.poses.append(poseTarget)
-    else:
-        while not numIntersect==0 :
-            if len(intersections[0])==2 :
-                index.append(0)
-                newP.append(recalc(intersections[0],Point(poseObs0.x,poseObs0.y), obstacles[0]))
-            if len(intersections[1])==2 :
-                index.append(1)
-                newP.append(recalc(intersections[1],Point(poseObs1.x,poseObs1.y), obstacles[1]))
-            if len(intersections[2])==2 :
-                index.append(2)
-                newP.append(recalc(intersections[2],Point(poseObs2.x,poseObs2.y), obstacles[2]))
-            if len(intersections[3])==2 :
-                index.append(3)
-                newP.append(recalc(intersections[3],Point(poseObs3.x,poseObs3.y), obstacles[3]))
-            if len(intersections[4])==2 :
-                index.append(4)
-                newP.append(recalc(intersections[4],Point(poseObs4.x,poseObs4.y), obstacles[4]))
-            if len(intersections[5])==2 :
-                index.append(5)
-                newP.append(recalc(intersections[5],Point(poseObs5.x,poseObs5.y), obstacles[5]))
-            if len(intersections[6])==2 :
-                index.append(6)
-                newP.append(recalc(intersections[6],Point(poseObs6.x,poseObs6.y), obstacles[6]))
-            if len(intersections[7])==2 :
-                index.append(7)
-                newP.append(recalc(intersections[7],Point(poseObs7.x,poseObs7.y), obstacles[7]))
-            print "New points calced: ", newP
-            puntosOrdenados= arrange(pointSelf)
-            ntraj=[]
-            ntraj.append(pointSelf)
-            for x in range(len(puntosOrdenados)):
-                poseAdd= init_pose()
-                laux=newP[puntosOrdenados[x]]
-                poseAdd.x=int(laux[0].x)
-                poseAdd.y= int(laux[0].y)
-                poseAdd.theta= 0
-                ntraj.append(laux[0])
-                arr.poses.append(poseAdd)
-
-            ntraj.append(pointTarget)
+        if numIntersect==0:
             arr.poses.append(poseTarget)
-
-            nintersect=[]
-            numIntersect=0
-            # for x in xrange(len(puntosOrdenados)):
-            #     seg= Segment(ntraj[x],ntraj[x+1])
-            #     nintersect= calcIntersect(obstacles, seg)
-            #     numIntersect= numIntersect+len(nintersect[0])+len(nintersect[1])+len(nintersect[2])+len(nintersect[3])+len(nintersect[4])+len(nintersect[5])+len(nintersect[6])+len(nintersect[7])
+        else:
+            while not numIntersect==0 :
+                newP=[]
+                index=[]
+                if len(intersections[0])==2 :
+                    index.append(0)
+                    newP.append(recalc(intersections[0],Point(poseObs0.x,poseObs0.y), obstacles[0]))
+                if len(intersections[1])==2 :
+                    index.append(1)
+                    newP.append(recalc(intersections[1],Point(poseObs1.x,poseObs1.y), obstacles[1]))
+                if len(intersections[2])==2 :
+                    index.append(2)
+                    newP.append(recalc(intersections[2],Point(poseObs2.x,poseObs2.y), obstacles[2]))
+                if len(intersections[3])==2 :
+                    index.append(3)
+                    newP.append(recalc(intersections[3],Point(poseObs3.x,poseObs3.y), obstacles[3]))
+                if len(intersections[4])==2 :
+                    index.append(4)
+                    newP.append(recalc(intersections[4],Point(poseObs4.x,poseObs4.y), obstacles[4]))
+                if len(intersections[5])==2 :
+                    index.append(5)
+                    newP.append(recalc(intersections[5],Point(poseObs5.x,poseObs5.y), obstacles[5]))
+                if len(intersections[6])==2 :
+                    index.append(6)
+                    newP.append(recalc(intersections[6],Point(poseObs6.x,poseObs6.y), obstacles[6]))
+                if len(intersections[7])==2 :
+                    index.append(7)
+                    newP.append(recalc(intersections[7],Point(poseObs7.x,poseObs7.y), obstacles[7]))
+                print "New points calced: ", newP
+                puntosOrdenados= arrange(pointSelf, newP)
+                ntraj=[]
+                ntraj.append(pointSelf)
+                for x in range(len(puntosOrdenados)):
+                    poseAdd= init_pose()
+                    laux=newP[puntosOrdenados[x]]
+                    poseAdd.x=int(laux[0].x)
+                    poseAdd.y= int(laux[0].y)
+                    poseAdd.theta= 0
+                    ntraj.append(laux[0])
+                    arr.poses.append(poseAdd)
+                ntraj.append(pointTarget)
+                arr.poses.append(poseTarget)
+                # nintersect=[]
+                # for x in xrange(len(puntosOrdenados)):
+                #     seg= Segment(ntraj[x],ntraj[x+1])
+                #     nintersect= calcIntersect(obstacles, seg)
+                #     numIntersect=  intDiscriminator(nintersect)
+                numIntersect=0
 
         print "The array is:", arr
         pub.publish(arr)
-        while True:
-            b=1
-        #rate.sleep()
+        # while True:
+        #     b=1
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
